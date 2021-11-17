66d7e55d3af532e4183e28c6969d22f24dc0a811
vinid <chiccobia@gmail.com>
2021-11-17 17:38:26 +0100

    now if not logged with automatically login

--- 1passel.el
+++ 1passel.el
@@ -27,15 +27,29 @@
 (defun 1passel-extract-loop (json-data)
   "Filters the JSON coming from 1password; extracts the name of the account (title) and the UUID"  
   (cl-loop for x in json-data
-         collect (cons (gethash "title" (gethash "overview" x)) (gethash "uuid" x))))
+	   collect (cons (gethash "title" (gethash "overview" x)) (gethash "uuid" x))))
+
+(shell-command-to-string (format "op list items"))
+
+(defun json-or-login ()
+  (let* ((json (shell-command-to-string (format "op list items --session=%s" 1passel-session))))
+    (if (string-match-p (regexp-quote "ERROR") json)
+	(progn
+	  (1passel-login)
+	  (json-read-from-string (shell-command-to-string (format "op list items --session=%s" 1passel-session))))
+      (json-read-from-string json))))
+
+
+
 
 (defun 1passel-get-password ()
   "Prompts for a list of accounts. Once one is selected, the password is extracted from 1password"
   (interactive)
+  
   (let* ((json-object-type 'hash-table)
 	 (json-array-type 'list)
 	 (json-key-type 'string)
-	 (json (json-read-from-string (shell-command-to-string (format "op list items --session=%s" 1passel-session))))
+	 (json (json-or-login))
 	 (filtered-ids (1passel-extract-loop json)))
     (string-trim
      (kill-new
@@ -48,7 +62,6 @@
 
 (defun 1passel-login ()
   "Creates a new login session for 1password. Stores the session in the variable 1passel-session"  
-  (interactive)
   (setq 1passel-session
 	(string-trim
 	 (shell-command-to-string   
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 1passel is a very simple tool to integrate 1password into emacs. I find this useful to be used within the EXWM desktop environment. With this I can get my password anywhere with a few keystrokes.
 
-
-
 ## Installation
 
 The main prerequisite for this package is the `op` command-line utility provided by [1password](https://1password.com/downloads/command-line/). One this is installed, you can download the 1passel.el file and load it from your init file.
+
+You also need to have gpg-agent running as the tool is going to use your master password stored in an encrypted gpg file (see this [line](https://github.com/vinid/1passel/blob/master/1passel.el) of code).
 
 
 A part from the usual installation you can install this using [straight](https://github.com/raxod502/straight.el) and [use-package](https://github.com/jwiegley/use-package).
